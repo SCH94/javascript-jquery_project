ff1359237ddf8c5416459a98af546278a0cc5ac5
SCH94 <arul1694@gmail.com>
2016-03-19 20:09:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
-$(document).ready(function(){
-var start = function(option){
-	$('#cont').html("");
 
-	var input = prompt('Type square 1 and 100');
+  var makeGrid = function(){
+    $('#Container').html("");
 
-	if (input >= 1 && input <= 100){	// Check validity of input
-		var sqr_size = $('#cont').width()/input - 2;	
+    var input = prompt("choose between 1 and 100 ");
 
-		for(var i = 1; i <= input; i++){
-			for(var j = 1; j <= input; j++){
-				$('#cont').append('<div class="sqr"></div>');
-			}
-			$('#cont').append('<div class="nsqr"></div>');
-		}
-		$('.sqr').css('width',sqr_size);
-		$('.sqr').css('height',sqr_size);
+      var Size = $('#Container').width()/input - 2;
 
-		$('.sqr').hover(function(){
-			switch(option){
-				case 1:
-					$(this).addClass('act');
-					break;
-				case 2:
-					$(this).addClass('act2');
-					}
-					break;
-			
-			}
-		});
-	}
-	else{
-		window.alert('Plz,Dont Leave.');
-	}
+      for(var i = 1; i <= input; i++){
+        for(var j = 1; j <= input; j++){
+          $('#Container').append('<div class="square"></div>');
+        };
+      }
+      $('.square').css('width',Size);
+      $('.square').css('height',Size);
+
+      $('.square').mouseenter(function(){
+            $(this).addClass('square_c');
+
+      });
 }
-});
+
