0ac8155792216301c452ca0cb54616bd915fe822
SCH94 <arul1694@gmail.com>
2016-03-19 17:43:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-var strt = function(option){
-$('#cont').html("");
-var input = prompt('Type Number of squares');
+var start = function(option){
+	$('#cont').html("");
 
-var sqrs = $('#cont').width()/input - 2; 
-if (input >= 1 && input <= 50){ 
-for(var i=1;i<=input,i++){
-	for(var j =1;j<=input;j++){
-		$('#cont').append("<div class='sqr'></div>");
+	var input = prompt('Type an integer between 1 and 128');
+
+	if (input >= 1 && input <= 128){	// Check for non-ridiculous input
+		var sqr_size = $('#cont').width()/input - 2;	// The -2 accounts for border size
+
+		for(var i = 1; i <= input; i++){
+			for(var j = 1; j <= input; j++){
+				$('#cont').append('<div class="sqr"></div>');
+			}
+			$('#cont').append('<div class="nsqr"></div>');
+		}
+		$('.sqr').css('width',sqr_size);
+		$('.sqr').css('height',sqr_size);
+
+		$('.sqr').hover(function(){
+			switch(option){
+				case 1:
+					$(this).addClass('act');
+					break;
+				case 2:
+					$(this).addClass('act2');
+					}
+					break;
+			
+			}
+		});
 	}
-	$('#cont').append('<div class="nsqr"></div>');
+	else{
+		alert('Plz,Dont Leave.');
+	}
 }
-$('.sqr').css('width',sqrs);
-$('.sqr').css('height',sqrs);
-
-$('.sqr').hover(function(){
-	switch(option){
-		case 1:
-	$(this).addClass('act');
-	break;
-	case 2:
-	$(this).addClass('act2');
-	break;
-	}
-	});
-}else{
-		alert('srry');
-	}
-
-};
