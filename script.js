2a4329416b040d58b57a52c6cb4a0cb342219e82
SCH94 <arul1694@gmail.com>
2016-04-01 22:00:53 +0530

    Added color selector.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-var squareGrid = function(){
+var squareGrid = function() {
     $('#gridContainer').html("");
 
     var input = prompt("Enter a number between 1 and 100.");
+    var color = prompt("Enter a  Color i.e red,blue,black,yellow");
+    var clr = color.toLowerCase();
 
       var squareSize = $('#gridContainer').width()/input - 2;
 
-      for(var i = 1; i <= input; i++){
-        for(var j = 1; j <= input; j++){
+      for (var i = 1; i <= input; i++) {
+        for (var j = 1; j <= input; j++) {
           $('#gridContainer').append('<div class="square"></div>');
         };
       }
@@ -14,7 +16,18 @@ var squareGrid = function(){
       $('.square').css('height',squareSize);
 
       $('.square').mouseenter(function(){
-            $(this).addClass('square_change');
-
+         if (clr==="black") {
+          $(this).addClass('black');
+          }
+          else if (clr==="red") {
+          $(this).addClass('red');
+          }
+          else if (clr==="blue") {
+          $(this).addClass('blue');
+          }
+          else if (clr==="yellow") {
+          $(this).addClass('yellow');
+          }
       });
 }
+
